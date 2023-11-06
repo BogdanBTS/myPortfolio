f409e1ed9ac33d8f416c6283fb2b9ebd44fead2e
Bogdan Tsaruk <u.s.a.bogdan@gmail.com>
2023-11-06 23:33:19 +0100

    Add Skills.jsx and Skills.module.css to the components folder

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Skills.module.css'
+
+function Skills() {
+    return (
+        <section className={styles.skills} id='skills'>
+
+        </section>
+    )
+}
+
+export default Skills
